f30513afe62d7c5ca94692ad43eb474edd2d544c
Jonas <jonas@oisann.net>
2014-09-18 23:27:44 +0200

    Testing weather api + minor changes
    Minor changes to the login module. More will come...

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -15,5 +15,57 @@ app.controller('StihkController', ['$scope', 'socket', function($scope, socket){
 }]);
 
 $(document).ready(function() {
-  console.log('Loaded!');
+  displayFeed($(window).width());
+  getWeather();
+  $(window).resize(function() {
+    displayFeed($(this).width());
+  });
 });
+
+var displayFeed = function(width) {
+  if($('#feed').length <= 0) {
+    if(width >= 980) {
+      $('body').append('<div class="navbar-default" id="feed"></div>');
+      $('#cont').removeClass('nofeed');
+    }
+  } else {
+    if(width <= 980) {
+      $('#feed').remove();
+      $('#cont').addClass('nofeed');
+    }
+  }
+}
+
+var getWeather = function() {
+    $.getJSON('api/weather/local', function(json){
+      updateWeather(json);
+    }).fail(function() {
+      updateWeather();
+    });
+}
+
+var updateWeather = function(data) {
+  var div = $('header #weather');
+  if(data === undefined) {
+      div.hasClass('hidden') ? console.log('Weather failed') : div.addClass('hidden');
+      return;
+  }
+  if(div.hasClass('hidden')) {
+    div.animate({ opacity: 0.0 }, 1, function() {
+      div.find('.trondheim .weather').text(data.trondheim.temp);
+      div.find('.korsvegen .weather').text(data.korsvegen.temp);
+      div.find('.trondheim').attr('title', data.trondheim.title);
+      div.find('.korsvegen').attr('title', data.korsvegen.title);
+      div.removeClass('hidden');
+      div.animate({ opacity: 1.0 }, 1000);
+    });
+  } else {
+    div.animate({ opacity: 0.0 }, 1000, function() {
+      div.find('.trondheim .weather').text(data.trondheim.temp);
+      div.find('.korsvegen .weather').text(data.korsvegen.temp);
+      div.find('.trondheim').attr('title', data.trondheim.title);
+      div.find('.korsvegen').attr('title', data.korsvegen.title);
+      div.animate({ opacity: 1.0 }, 1000);
+    });
+  }
+}
